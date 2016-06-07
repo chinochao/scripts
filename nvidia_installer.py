66da06a4b9bff2a5d840870001811940e680692a
Roberto Chaud <chinochao@Robertos-MBP.wpa.txstate.edu>
2016-06-07 12:52:26 -0500

    support keyboard interrupts

--- nvidia_installer.py
+++ nvidia_installer.py
@@ -27,23 +27,26 @@ def progress_bar(count, blockSize, totalSize):
 
 
 if __name__ == '__main__':
-  print('[*]NVidia Drivers Setup')
-  print('[**]Latest versions available are: \nStable: %s \nBeta: %s' % (latest_ver, beta_ver))
-  version = input('Press [S] for Stable and [B] for Beta version: ')
-  while True:
-    if version.upper() == 'S':
-      dl_ver = latest_ver
-      dl_url = latest_url
-      dl_file = latest_file
-      break
-    elif version.upper() == 'B':
-      dl_ver = beta_ver
-      dl_url = beta_url
-      dl_file = beta_file
-      break
-    else:
-      version = input('Press [S] for Stable and [B] for Beta version: ')
-  print('[**]Downloading %s' % (dl_ver))
-  dl_loc = ('%s/Downloads/%s' % (os.environ['HOME'], dl_file))
-  req.urlretrieve(dl_url, dl_loc, reporthook=progress_bar)
-  print('\n[**]NVidia Drivers downloaded: %s' % (dl_loc))
+  try:
+    print('[*]NVidia Drivers Setup')
+    print('[**]Latest versions available are: \nStable: %s \nBeta: %s' % (latest_ver, beta_ver))
+    version = input('Press [S] for Stable and [B] for Beta version: ')
+    while True:
+      if version.upper() == 'S':
+        dl_ver = latest_ver
+        dl_url = latest_url
+        dl_file = latest_file
+        break
+      elif version.upper() == 'B':
+        dl_ver = beta_ver
+        dl_url = beta_url
+        dl_file = beta_file
+        break
+      else:
+        version = input('Press [S] for Stable and [B] for Beta version: ')
+    print('[**]Downloading %s' % (dl_ver))
+    dl_loc = ('%s/Downloads/%s' % (os.environ['HOME'], dl_file))
+    req.urlretrieve(dl_url, dl_loc, reporthook=progress_bar)
+    print('\n[**]NVidia Drivers downloaded: %s' % (dl_loc))
+  except KeyboardInterrupt:
+    print('Exiting ...')
